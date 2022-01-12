836731d4aef84c7fe4cb4d1c3463418977642456
Thomas Trouton <thomastrouton@gmail.com>
2022-01-12 12:04:29 +0000

    refactor python script

--- update_srcset.py
+++ update_srcset.py
@@ -1,35 +1,26 @@
 import glob
 from PIL import Image
 import re
+from bs4 import BeautifulSoup
 
 def get_srcset(path):
     images = glob.glob("./" + path.replace("original","*").replace(".jpg",".*").replace(".jpeg",".*"))
     results =[]
     for img in images:
         width = Image.open(img).width
-        print("found:" + img + " with width: " + str(width))
         results.append((width, img))
     results.sort()
-    srcset = "srcset=\"" + ", ".join([img + " " + str(px) + "w" for px, img in results])
-    srcset = srcset + "\""
-    return srcset
+    return ", ".join([img + " " + str(px) + "w" for px, img in results])
 
 
 if __name__ == "__main__":
 
     a_file = open("index.html", "r")
-    list_of_lines = a_file.readlines()
+    soup = BeautifulSoup(a_file.read(), 'html.parser')
     a_file.close()
-    result_lines = []
-    for line in list_of_lines:
-        if "<img" in line and "src=" in line:
-            line = re.sub("srcset=\".*?\"", "",line)
-            path = line.split("src=\"")[1].split("\"")[0]
-            line = line.replace("src=", get_srcset(path) + " src=" )
-            result_lines.append(line)
-        else:
-            result_lines.append(line)
-    
+    for img in soup.find_all('img'):
+        srcset = get_srcset(img.get('src'))
+        img["srcset"] = srcset
     output_file = open("index.html","w")
-    output_file.writelines(result_lines)
+    output_file.write(soup.prettify(formatter="html5"))
     output_file.close()
